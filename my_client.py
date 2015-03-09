ab9661f58c5a54d607a2cd20496848833ed12339
alex <akmail255@gmail.com>
2015-03-09 21:52:14 +0300

    Add broadcasting.

--- my_client.py
+++ my_client.py
@@ -1,20 +1,71 @@
+import random
 import socket
 import sys
+from threading import Thread
 
-HOST = '127.0.0.1'
-PORT = 5555
+# Making Thread-object for thread that receives messages from the server.
+class clientThread(Thread):
+    def __init__(self, user_key):
+        Thread.__init__(self)
+        self.flag = True # Theard receives messages while this flag is True
+        self.user_key = user_key
 
-sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def run(self):
+        sock_r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_r.connect((HOST,PORT))
+        sock_r.settimeout(1)
+        sock_r.send('RECV-' + user_key)
+        #print("FFF", HOST, PORT, self.flag)
+        while self.flag:
+            try:
+                message = sock_r.recv(4096)
+                print message
+                if not message:
+                    self.flag = False
+            except:
+                pass
+        sock_r.close()
+
+
+#HOST = '127.0.0.1'
+#PORT = 5555
 try:
-    sock1.connect((HOST, PORT))
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+except:
+    print "Give me HOST and PORT in command line (after script name)."
+    # Example: python my_client.py 192.168.0.68 5555
+    sys.exit()
+    
+#HOST = '127.0.0.1'
+#PORT = 5555
+
+#Special symbols for erasing last line on the terminal window:
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+
+# This random number is our identifier for the server:
+user_key = str(random.randint(1000000,9000000))
+sock_w = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    sock_w.connect((HOST, PORT))
+    sock_w.send('SEND-' + user_key)
 except:
     print("Can't connect to server (%s:%s)." % (HOST, PORT))
     sys.exit()
 
+t_read = clientThread(user_key)
+t_read.start()
 
 try:
     while 1:
-        sock1.send(raw_input("Send:"))
+        sock_w.send(raw_input(""))
+        #print(CURSOR_UP_ONE + ERASE_LINE +'\e[#M') 
 except:
+    sock_w.close()
+    t_read.flag = False
+    t_read.join()
     print("closing...")
-    sock1.close()
+    sys.exit()
+    #sock_r.close()
--- my_server.py
+++ my_server.py
@@ -2,8 +2,27 @@ import socket
 import select
 import sys
 
-HOST = '127.0.0.1'
-PORT = 5555
+
+def broadcast(list_of_sockets, message):
+    for sock in list_of_sockets:
+        try:
+            sock.send(message)
+        except:
+            pass
+
+
+#HOST = '127.0.0.1'
+#HOST = '192.168.0.68'
+#PORT = 5555
+
+try:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+except:
+    print "Give me HOST and PORT in command line (after script name)."
+    # Example: python my_server.py 192.168.0.68 5555
+    sys.exit()
+
 
 
 serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,11 +31,15 @@ serversock.listen(15)
 serversock.setblocking(1)
 
 readlist = [serversock]
-#print("dir(serversock):", dir(serversock), "\n", serversock.fileno)
+writelist = []
+print "Chat-server was started."
+
 
 while 1:
     try:
+        #print "LLL"
         s_read, s_write, s_err = select.select(readlist, [], [])
+        #print "BBBB"
     except socket.error as error:
         print("Error number:", error.errno)
         serversock.close()
@@ -24,36 +47,54 @@ while 1:
     
     #print("s_read:", s_read, "\ns_write:", s_write, "\ns_err:", s_err)
     #print("AAA - readlist:", readlist)
+    #print "AAA"
     for sock in s_read:
         if sock == serversock:
+            #print "OOO"
             newsock, address = serversock.accept()
             newsock.setblocking(1)
-            readlist.append(newsock)
-            newsock.send("you're connected to the select server")
-            message = "Client %s : %s is online." % address
-            print(message)
+            recv_msg = newsock.recv(1024)
+            if recv_msg[:4] == 'SEND':
+                readlist.append(newsock)
+                message = "Client %s:%s is online." % address
+                broadcast(writelist, message)
+                print(message)
+            else:
+                writelist.append(newsock)
+                newsock.send("you're connected to the server")
+                #print "Writelist:", newsock, dir(newsock),"\n", newsock.__doc__
+            #print("MMM: s_read:", s_read, "\ns_write:", s_write, "\ns_err:", s_err)
+            #print("MMM1: readlist:", readlist, "writelist:", writelist)
             #broadcast(serversock, readlist, message)
             #print("BBB - readlist:", readlist)
         else:
+            #print "UUU"
             try:
                 recv_msg = sock.recv(1024)
-            
+                #print "YYY"
                 if recv_msg == "":
                     #print("DDD")
                     (remoute_host, remoute_port) = sock.getpeername()
-                    print "Client %s:%s closed the connection" %(remoute_host, 
-                                                                remoute_port)
+                    message = "Client %s:%s closed the connection" %(remoute_host, 
+                                                                     remoute_port)
+                    print message
+                    broadcast(writelist, message)
                     sock.close()
                     readlist.remove(sock)
                 else:
                     #print("FFF")
                     (remoute_host, remoute_port) = sock.getpeername()
-                    print "Client %s : %s sent: %s" %(str(remoute_host),
+                    message = "Client %s:%s : %s" %(str(remoute_host),
                                                     str(remoute_port),
                                                     str(recv_msg))
+                    print message
+                    broadcast(writelist, message)
+                    
+
             except:
                 print("Client %s : %s is offline." % sock.getpeername())
-                sock.close()
-                readlist.remove(sock)
+                print("NNN: s_read:", s_read, "\ns_write:", s_write, "\ns_err:", s_err)
+                print("NNN1: readlist:", readlist, "writelist:", writelist)
+                #sock.close()
+                #readlist.remove(sock)
                 
-
